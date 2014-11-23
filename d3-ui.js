ada591d5dfa73cf83ce76596cccc7b65c22ee0f9
Liji Jinaraj <liji@vunite.com>
2014-11-23 15:07:59 -0800

    tabs to spaces

--- d3-ui.js
+++ d3-ui.js
@@ -1,66 +1,66 @@
 !function(d3) {
-	'use strict';
+  'use strict';
 
-	if (typeof d3 === 'undefined') {
-	  throw new Error('d3-ui requires D3 http://d3js.org/')
-	}
+  if (typeof d3 === 'undefined') {
+    throw new Error('d3-ui requires D3 http://d3js.org/')
+  }
 
-	d3.ui = {
-		version: "1.0.0"
-	}
+  d3.ui = {
+    version: "1.0.0"
+  }
 
-	d3.ui.alert = function() {
-		var TRANSITION_DURATION = 150;
-		var dismiss = '[data-dismiss="alert"]'
+  d3.ui.alert = function() {
+    var TRANSITION_DURATION = 150;
+    var dismiss = '[data-dismiss="alert"]'
 
-		var alert = function(el) {
-			if(!el.selectAll(dismiss).empty()) { 
-				el.on('click', closeHandler) 
-			}
+    var alert = function(el) {
+      if(!el.selectAll(dismiss).empty()) { 
+        el.on('click', closeHandler) 
+      }
 
-			return alert
-		}
+      return alert
+    }
 
-		alert.close = function(el) {
-			var closeEvent = new Event('close.bs.alert')
-			
-			el.node().dispatchEvent(closeEvent)
+    alert.close = function(el) {
+      var closeEvent = new Event('close.bs.alert')
+      
+      el.node().dispatchEvent(closeEvent)
 
-			el.classed('in', false)
+      el.classed('in', false)
 
-			el.transition()
-				.duration(el.classed('fade') ? TRANSITION_DURATION : 0)
-				.each('end', function() {
-					var closedEvent = new Event('closed.bs.alert')
-					el.node().dispatchEvent(closedEvent)
-				})
-				.remove()
+      el.transition()
+        .duration(el.classed('fade') ? TRANSITION_DURATION : 0)
+        .each('end', function() {
+          var closedEvent = new Event('closed.bs.alert')
+          el.node().dispatchEvent(closedEvent)
+        })
+        .remove()
 
-			return alert
-		}
+      return alert
+    }
 
-		function closeHandler() {
-			var el = d3.select(this);
-			
-			if(!el.classed('alert') || !dismissable(d3.event.target)) {
-				return
-			}
+    function closeHandler() {
+      var el = d3.select(this);
+      
+      if(!el.classed('alert') || !dismissable(d3.event.target)) {
+        return
+      }
 
-			d3.event.preventDefault()
+      d3.event.preventDefault()
 
-			alert.close(el)
-		}
+      alert.close(el)
+    }
 
-		function dismissable(target) {
-			try {
-				return target.parentNode.dataset.dismiss === 'alert'
-			} catch(e) {
-				return false
-			}
-		}
+    function dismissable(target) {
+      try {
+        return target.parentNode.dataset.dismiss === 'alert'
+      } catch(e) {
+        return false
+      }
+    }
 
-		d3.selectAll(".alert.alert-dismissible").call(alert)
+    d3.selectAll(".alert.alert-dismissible").call(alert)
 
-		return alert
-	}()
+    return alert
+  }()
 }(d3);
